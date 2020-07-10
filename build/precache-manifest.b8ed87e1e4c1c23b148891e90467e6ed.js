5275491dbb260b86e806496e4df7bc78da4b747a
alexombre <moutapamalexandre@gmail.com>
2020-07-10 13:40:28 +0200

    deployement branch

---
+++ build/precache-manifest.b8ed87e1e4c1c23b148891e90467e6ed.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "032625a2495d11f2072284defb322cd9",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "cb4c01478dbd95a8d248",
+    "url": "/portfolio/static/css/2.9edc93b8.chunk.css"
+  },
+  {
+    "revision": "0fd6d720d1c59cb270fc",
+    "url": "/portfolio/static/css/main.3010ff2f.chunk.css"
+  },
+  {
+    "revision": "cb4c01478dbd95a8d248",
+    "url": "/portfolio/static/js/2.6d9690c8.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/portfolio/static/js/2.6d9690c8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0fd6d720d1c59cb270fc",
+    "url": "/portfolio/static/js/main.914f6404.chunk.js"
+  },
+  {
+    "revision": "7a6cc99bd8f08f9fa6ec",
+    "url": "/portfolio/static/js/runtime-main.209c2d7e.js"
+  },
+  {
+    "revision": "ab78bb626dfb0bf69da2cfda7e020e59",
+    "url": "/portfolio/static/media/bootstrap.ab78bb62.png"
+  },
+  {
+    "revision": "166b2fe7c31a0369b9be00c9bf87e3a2",
+    "url": "/portfolio/static/media/progressive_app.166b2fe7.png"
+  },
+  {
+    "revision": "9510543aa645f6e72a7ac76b51fde3b8",
+    "url": "/portfolio/static/media/react-native-2.9510543a.png"
+  },
+  {
+    "revision": "0ad1d17f18f25d10000c84bc82e7bd9f",
+    "url": "/portfolio/static/media/react.0ad1d17f.png"
+  },
+  {
+    "revision": "eaf354267b0a2ccdf5b016ff0f690a30",
+    "url": "/portfolio/static/media/ror.eaf35426.png"
+  },
+  {
+    "revision": "3948ed367e5e82b78418ae9e3bd34253",
+    "url": "/portfolio/static/media/webdev.3948ed36.png"
+  }
+]);
